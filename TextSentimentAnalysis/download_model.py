745a0677f9b0ccdaf8e47659b8915ad4e8932807
ChiyukiKazama <1765154918@qq.com>
2026-03-07 20:33:03 +0800

    Add files via upload

---
+++ TextSentimentAnalysis/download_model.py
@@ -0,0 +1,8 @@
+from modelscope import snapshot_download
+
+model_dir = snapshot_download(
+    model_id="qwen/Qwen-7B-Chat-Int4",
+    cache_dir="./models/Qwen-7B-Chat-Int4",  # 保存路径
+    revision="master"
+)
+print(f"模型已下载到：{model_dir}")
